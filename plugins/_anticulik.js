59aef92fec5c62825764fe05f4e2acf16c2ef3e8
M imam Adi <84767049+Adi-OfficialL@users.noreply.github.com>
2021-09-11 16:45:02 +0700

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -3,7 +3,7 @@ let handler = m => m
 handler.all = async function (m) {
     if (m.mtype === 'groupInviteMessage') {
         m.reply(`untuk mengundang bot ke dalam grup silahkan Ijin Owner terlebih dahulu`)
-        this.sendContact(m.chat, '33623746892', 'ᴀᴅɪ ᴏғғɪᴄɪᴀʟあ', m)
+        this.sendContact(m.chat, '6289504585790', 'ᴀᴅɪ ᴏғғɪᴄɪᴀʟあ', m)
     }
 }
 
