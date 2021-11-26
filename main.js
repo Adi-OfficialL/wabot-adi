3f189f217372138384adc4564262fcfd98e4babf
M imam Adi <84767049+Adi-OfficialL@users.noreply.github.com>
2021-11-27 00:12:43 +0700

    New Version

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ if (!global.DATABASE.data.stats) global.DATABASE.data.stats = {}
 if (!global.DATABASE.data.stats) global.DATABASE.data.msgs = {}
 if (!global.DATABASE.data.sticker) global.DATABASE.data.sticker = {}
 global.conn = new WAConnection()
-conn.version = [2, 2143, 3]
+conn.version = [2, 2142, 12]
 let authFile = `${opts._[0] || 'session'}.data.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
